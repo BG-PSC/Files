1d2ba6d181d4fd2998bbfdf74723927eb6757c91
PLudrak <jakwojcicki97@gmail.com>
2025-11-19 10:18:44 +0100

    poprawa struktury

---
+++ delete_nodata.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import os
+
+tile_dir = r"D:\kuba\obwodnica_sztabina\tiles"
+
+for root, dirs, files in os.walk(tile_dir):
+    for file in files:
+        if file.endswith(".png"):
+            img = Image.open(os.path.join(root, file))
+            if img.getextrema() == (
+                (0, 0),
+                (0, 0),
+                (0, 0),
+                (0, 0),
+            ):  # Wszystkie kanały 0 (czarny)
+                os.remove(os.path.join(root, file))  # Usuń czarny tile
+                print(f"No data tile {os.path.join(root,file)} deleted")
