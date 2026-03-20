d26d332e5862e4f284042c1833305c93c75f2969
PLudrak <jakwojcicki97@gmail.com>
2026-03-20 13:15:46 +0100

    new zoom level for s19

--- delete_nodata.py
+++ delete_nodata.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os
 
-tile_dir = r"D:\kuba\obwodnica_sztabina\tiles"
+tile_dir = r"D:\lokalne_backup\S19 orto\tiles"
 
 for root, dirs, files in os.walk(tile_dir):
     for file in files:
