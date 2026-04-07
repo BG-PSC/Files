e524ea257fe309bb356b1e27d841ea89e5a933b3
PLudrak <jakwojcicki97@gmail.com>
2026-04-07 07:56:57 +0200

    skrypt do zmniejszania zdjęć

--- resize_photos.py
+++ resize_photos.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 # Ścieżki absolutne (zmień na swoje!)
-input_folder = r"Z:\roboty\STABILIZACJA\S19\zdjęcia Sławek\S19_IWONICZ-CZ1-ZDJ+ZES\ZDJECIA-CZ1"
+input_folder = r"Z:\roboty\STABILIZACJA\S19\zdjęcia Sławek\S19_IWONICZ-CZ2B-ZDJ+ZES"
 output_folder = r"D:\Python\kuba\web_map\Files\pliki\graniczniki\S19"
 
 # Utwórz folder wyjściowy, jeśli nie istnieje
@@ -12,10 +12,15 @@ os.makedirs(output_folder, exist_ok=True)
 # Obsługiwane formaty
 valid_extensions = (".jpg", ".jpeg", ".png", ".bmp", ".webp")
 
+
 for filename in os.listdir(input_folder):
     if filename.lower().endswith(valid_extensions):
         input_path = os.path.join(input_folder, filename)
         output_path = os.path.join(output_folder, filename)
+
+        if os.path.exists(output_path):
+            print(f"Pomijam {filename} (już istnieje w folderze docelowym)")
+            continue
 
         with Image.open(input_path) as img:
             width, height = img.size
@@ -30,5 +35,6 @@ for filename in os.listdir(input_folder):
 
             resized_img = img.resize((new_width, new_height), Image.LANCZOS)
             resized_img.save(output_path)
+            print(f"Zapisano {filename} do folderu docelowego")
 
 print("Gotowe!")
